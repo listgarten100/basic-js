862168fb3bc9a94e6021807756323eda3ef49cae
Vlad Listgarten <listgarten100@gmail.com>
2021-01-28 15:08:46 +0200

    feat: create dateSample function in carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let time = 0;
+  let radioConst = 0.693 / HALF_LIFE_PERIOD;
+
+  if(+sampleActivity !== 'number') return false;
+
+  time += (Math.log(MODERN_ACTIVITY/+sampleActivity)) / radioConst;
+
+ return Math.ceil(time);
 };
