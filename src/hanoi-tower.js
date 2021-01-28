18a5bc8720c7793a5ce58eb6502ea40cc666b9df
Vlad Listgarten <listgarten100@gmail.com>
2021-01-28 18:39:28 +0200

    feat: create function calculateHanoi in task hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,14 @@
 //start task hanoi
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+ 
+  let turns = Math.pow(2, disksNumber) - 1;
+  
+  let sec = Math.floor(((turns / turnsSpeed) * 3600));
+  
+  return {
+    turns: turns,
+    seconds: sec
+  };
 };
