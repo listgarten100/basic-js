ff2fde1bfbe6d2a9b8dc28b05f0b5ed7e2748ffd
Vlad Listgarten <listgarten100@gmail.com>
2021-01-28 16:08:02 +0200

    feat: create dreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,5 +2,24 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam( members ) {
- 
+  let res = '';
+  let sortTeam;
+  let arr = [];
+
+  if(!Array.isArray(members)) return false;
+
+  members.forEach(elem => {
+    if(typeof(elem) !== 'string') res += '';
+    else{
+    arr.push(elem.trim().toUpperCase().slice(0,1));
+    }
+  });
+
+  sortTeam = arr.sort();
+
+  sortTeam.forEach(elem => {
+    res += elem;
+  });
+
+  return res;
 };
