6719934d7c3bebf73331e8c11b8c1112803e740f
Vlad Listgarten <listgarten100@gmail.com>
2021-01-29 14:07:10 +0200

    feat: create transform function in transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,5 +2,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  
+  let res = [];
+
+  if(!Array.isArray(arr)) {
+    throw new Error('Error');
+  }
+
+  arr.forEach((element, i) => {
+
+    if(element === '--double-next'&& arr[i+1] === undefined || element === '--double-prev'&& arr[i-1] === undefined || element === '--discard-next'&& arr[i+1] === undefined || element === '--discard-prev' && arr[i-1] === undefined) return;
+    
+    else{
+
+      if(element === '--double-next') res.push(arr[i+1]);
+      
+      else if(element === '--double-prev') {
+      if(arr[i-2] === '--discard-next') return;
+      res.push(arr[i-1]);
+      } 
+
+      else if(arr[i-1] === '--discard-next') res.pop();
+
+      else if(element === '--discard-prev') {
+      if(arr[i-2] === '--discard-next') return;
+      res.pop();
+     }
+      else res.push(element);
+    }
+  });
+  return res;
 };
